e689abac4700861d1abab7d1a7691f27e64146ad
Martin Gendell <martin@gendell.com>
2015-02-22 17:12:32 -0500

    Initial Commit

---
+++ UITableViewController Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge
+//
+//  Created by Martin Gendell on 2/22/15.
+//  Copyright (c) 2015 MAGnetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge
+//
+//  Created by Martin Gendell on 2/22/15.
+//  Copyright (c) 2015 MAGnetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
